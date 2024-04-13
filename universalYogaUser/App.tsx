a370288e5997db128904bb8ca2f645b9d518bb70
vinaysai924 <Vc5522d@gre.ac.uk>
2024-04-13 06:53:44 +0100

    Add files via upload

---
+++ universalYogaUser/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Navigate from './src/navigate/NavigateIndex';
+import LoginScreen from './src/component/loginIn/LoginScreen';
+const Apps = () => {
+  return <Navigate />;
+};
+
+export default Apps;
